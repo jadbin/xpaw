8eaffd4e55d3aac99b508ecfd74ca46413709677
jadbin <jadbin.com@hotmail.com>
2017-02-20 09:04:29 +0800

    add template

--- xpaw/config/defaultconfig.py
+++ xpaw/config/defaultconfig.py
@@ -1,6 +1,11 @@
+# coding=utf-8
+
+from os.path import abspath, join, dirname
 
 LOG_FILE = None
 LOG_ENCODING = "utf-8"
 LOG_LEVEL = "INFO"
 LOG_FORMAT = "%(asctime)s %(name)s [%(levelname)s]: %(message)s"
 LOG_DATEFORMAT = "%Y-%m-%d %H:%M:%S"
+
+TEMPLATES_DIR = abspath(join(dirname(__file__), '..', 'templates'))
