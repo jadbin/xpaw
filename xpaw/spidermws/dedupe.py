89e2e4355f6903d9981cd4357be7eb963a7fbfbf
jadbin <jadbin.com@hotmail.com>
2017-03-14 15:22:00 +0800

    update mongo dedupe

--- xpaw/spidermws/dedupe.py
+++ xpaw/spidermws/dedupe.py
@@ -34,20 +34,19 @@ class MongoDedupeMiddleware(DedupeMiddleware):
     def __init__(self, mongo_addr, mongo_db, mongo_tbl):
         mongo_client = MongoClient(mongo_addr)
         self._dedupe_tbl = mongo_client[mongo_db][mongo_tbl]
-        self._dedupe_tbl.create_index("url")
+        self._dedupe_tbl.create_index("request")
 
     @classmethod
     def from_config(cls, config):
         task_id = config.get("task_id")
-        return cls(config.get("mongo_dedupe_addr"),
+        return cls(config.get("mongo_dedupe"),
                    "xpaw_dedupe",
                    "task_{0}".format(task_id))
 
     def _is_dupe(self, request):
-        url = request.url
-        res = self._dedupe_tbl.find_one({"url": url})
+        res = self._dedupe_tbl.find_one({"request": request.method + " " + request.url})
         if res is None:
-            self._dedupe_tbl.insert_one({"url": url})
+            self._dedupe_tbl.insert_one({"request": request.method + " " + request.url})
             return False
         return True
 
@@ -57,8 +56,7 @@ class LocalSetDedupeMiddleware(DedupeMiddleware):
         self._url_set = set()
 
     def _is_dupe(self, request):
-        url = request.url
         if url not in self._url_set:
-            self._url_set.add(url)
+            self._url_set.add(request.method + " " + request.url)
             return False
         return True
