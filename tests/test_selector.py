2602f59c9be37ee6308c154481700612c9ede92e
jadbin <jadbin.com@hotmail.com>
2017-11-02 12:55:29 +0800

    update test

--- tests/test_selector.py
+++ tests/test_selector.py
@@ -16,33 +16,48 @@ class TestSelector:
         assert s.xpath("//div").xpath(".//li").text[-1] == "c"
 
     def test_attribute_selection(self):
-        html = """<div style=display:none><a href="http://example.com/" target=_blank></div>"""
+        html = """<a href="http://example.com/" target=_blank>"""
         s = Selector(html)
-        assert s.xpath("//a/@href")[0].html == "http://example.com/"
-        assert s.xpath("//a").xpath("@target")[0].html == "_blank"
-        assert s.xpath("//div/@style")[0].html == "display:none"
+        assert s.xpath("//a/@href")[0].text == "http://example.com/"
+        assert s.xpath("//a/@target")[0].text == "_blank"
 
     def test_text_selection(self):
         html = """<div><p>expression: <var>x</var>+<var>y</var>=<var>z</var></p></div>"""
         s = Selector(html)
-        assert s.xpath("//var[last()-1]/text()")[0].html == "y"
-        assert s.xpath("//var/text()").html == ["x", "y", "z"]
+        assert s.xpath("//var/text()")[0].text == "x"
+        assert s.xpath("//var")[0].text == "x"
+        assert s.xpath("//var/text()").text == ["x", "y", "z"]
+        assert s.xpath("//var").text == ["x", "y", "z"]
         assert s.xpath("//p")[0].html == "<p>expression: <var>x</var>+<var>y</var>=<var>z</var></p>"
         assert s.xpath("//p")[0].text == "expression: x+y=z"
         assert s.xpath("//p/text()")[0].html == "expression: "
 
+    def test_node_selection(self):
+        html = """<p></p><p class='primary'>primary</p><p class="minor gray">minor</p>"""
+        s = Selector(html)
+        primary = s.xpath("//p[@class='primary']")
+        assert len(primary) == 1 and primary[0].text == 'primary'
+        minor = s.xpath("//p[@class='minor']")
+        assert len(minor) == 0
+        minor = s.xpath("//p[@class='gray minor']")
+        assert len(minor) == 0
+        minor = s.xpath("//p[@class='minor gray']")
+        assert len(minor) == 1 and minor[0].text == 'minor'
+        minor = s.xpath("//p[contains(@class, 'minor')]")
+        assert len(minor) == 1 and minor[0].text == 'minor'
+
     def test_encoding_detection(self):
         html = "<html lang=en><head>" \
                "<title>测试</title>" \
-               "<meta charset=utf-8>" \
+               "<meta charset=gbk>" \
                "<meta http-equiv=Content-Type content='text/html; charset=gbk' />" \
                "</head></html>"
         body = html.encode("gbk")
         with pytest.raises(UnicodeDecodeError):
-            s = Selector(body.decode("utf-8"))
+            Selector(body.decode("utf-8"))
         s = Selector(body.decode("ascii", errors="ignore"))
-        assert s.xpath("//meta/@charset")[0].html == "utf-8"
-        content_type = s.xpath("//meta[@http-equiv='Content-Type']").xpath("@content")[0].html
+        assert s.xpath("//meta/@charset")[0].text == "gbk"
+        content_type = s.xpath("//meta[@http-equiv='Content-Type']/@content")[0].text
         assert content_type == "text/html; charset=gbk"
         mtype, stype, _, params = parse_mimetype(content_type)
         assert mtype == "text" and stype == "html" and params.get("charset") == "gbk"
@@ -50,20 +65,18 @@ class TestSelector:
     def test_wrong_arguments(self):
         html = b"<html></html>"
         with pytest.raises(TypeError):
-            s = Selector(html)
+            Selector(html)
         with pytest.raises(ValueError):
-            s = Selector()
+            Selector()
 
-    def test_location_path(self):
-        html = "<p>text</p><ul><p>header</p><li>a</li><li>b</li></ul>"
+    def test_node_context(self):
+        html = "<p>header</p><div><p>text</p></div>"
         s = Selector(html)
 
-        assert s.xpath("/li") == []
-        assert s.xpath("//li") != []
+        assert s.xpath("/p") == []
+        assert s.xpath("//p") != []
+        assert s.xpath("/html/body/p") != []
 
-        assert s.xpath("/ul") == []
-        assert s.xpath("/html/body/ul") != []
-
-        assert s.xpath("//ul").xpath("//p")[0].text == "text"
-        assert s.xpath("//ul").xpath(".//p")[0].text == "header"
-        assert s.xpath("//ul").xpath("./p")[0].text == "header"
+        assert s.xpath("//div").xpath("//p")[0].text == "header"
+        assert s.xpath("//div").xpath(".//p")[0].text == "text"
+        assert s.xpath("//div").xpath("./p")[0].text == "text"
