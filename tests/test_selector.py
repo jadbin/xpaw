4936b027c252b36e74d6970bb5f1062323a5fdf4
jadbin <jadbin.com@hotmail.com>
2017-11-04 13:48:47 +0800

    add css selector

--- tests/test_selector.py
+++ tests/test_selector.py
@@ -18,19 +18,25 @@ class TestSelector:
     def test_attribute_selection(self):
         html = """<a href="http://example.com/" target=_blank>"""
         s = Selector(html)
+        assert s.xpath('//a')[0].xpath('@href')[0].text == 'http://example.com/'
         assert s.xpath("//a/@href")[0].text == "http://example.com/"
         assert s.xpath("//a/@target")[0].text == "_blank"
 
-    def test_text_selection(self):
+    def test_text(self):
         html = """<div><p>expression: <var>x</var>+<var>y</var>=<var>z</var></p></div>"""
         s = Selector(html)
         assert s.xpath("//var/text()")[0].text == "x"
         assert s.xpath("//var")[0].text == "x"
         assert s.xpath("//var/text()").text == ["x", "y", "z"]
         assert s.xpath("//var").text == ["x", "y", "z"]
-        assert s.xpath("//p")[0].html == "<p>expression: <var>x</var>+<var>y</var>=<var>z</var></p>"
+        assert s.xpath("//p")[0].string == "<p>expression: <var>x</var>+<var>y</var>=<var>z</var></p>"
         assert s.xpath("//p")[0].text == "expression: x+y=z"
-        assert s.xpath("//p/text()")[0].html == "expression: "
+        assert s.xpath("//p/text()")[0].string == "expression: "
+
+        xs = Selector(html, doctype='xml')
+        assert xs.xpath("//p")[0].string == "<p>expression: <var>x</var>+<var>y</var>=<var>z</var></p>"
+        assert xs.xpath("//p")[0].text == "expression: x+y=z"
+        assert xs.xpath("//p/text()")[0].string == "expression: "
 
     def test_node_selection(self):
         html = """<p></p><p class='primary'>primary</p><p class="minor gray">minor</p>"""
@@ -71,12 +77,66 @@ class TestSelector:
 
     def test_node_context(self):
         html = "<p>header</p><div><p>text</p></div>"
+        xml = "<xml><p>header</p><div><p>text</p></div></xml>"
         s = Selector(html)
+        xs = Selector(xml, doctype='xml')
 
         assert s.xpath("/p") == []
         assert s.xpath("//p") != []
         assert s.xpath("/html/body/p") != []
+        assert xs.xpath('/p') == []
+        assert xs.xpath('//p') != []
+        assert xs.xpath('/xml/p') != []
 
         assert s.xpath("//div").xpath("//p")[0].text == "header"
         assert s.xpath("//div").xpath(".//p")[0].text == "text"
         assert s.xpath("//div").xpath("./p")[0].text == "text"
+
+
+class TestCssSelector:
+    def test_selector_list(self):
+        html = """<li>a</li><div><ul><li>b</li><li>c</li></ul></div><ul><li>d</li></ul>"""
+        s = Selector(html)
+        assert s.css("li")[0].text == "a"
+        assert s.css("li")[-1].text == "d"
+        assert s.css("div li").text[0] == "b"
+        assert s.css("div li").text[-1] == "c"
+
+    def test_node_selection(self):
+        s = Selector("""<html>
+                <body>
+                    <h1 class='header'><a href="http://httpbin.org/">httpbin</a></h1>
+                    <ul>
+                        <li><a class='primary link' href="http://httpbin.org/">Index Page</a></li>
+                        <li><a class='link' href="http://httpbin.org/ip">Returns Origin IP</a></li>
+                    </ul>
+                </body>
+                </html>""")
+        assert s.css('a')[0].text == 'httpbin'
+        assert s.css('a[class=link]')[0].text == 'Returns Origin IP'
+        assert s.css('a[class~=link]')[0].text == 'Index Page'
+        assert s.css('ul > a') == []
+        assert len(s.css('li > a')) == 2
+        assert s.css('ul a')[0].text == 'Index Page'
+        assert s.css('ul').css('a')[0].text == 'Index Page'
+        assert s.css('li > a:not([class~=primary])')[0].text == 'Returns Origin IP'
+
+    def test_attribute_selection(self):
+        s = Selector("""<html>
+                        <body>
+                            <h1 class='header'><a href="http://httpbin.org/">httpbin</a></h1>
+                            <ul>
+                                <li><a class='primary link' href="http://httpbin.org/">Index Page</a></li>
+                                <li><a class='link' href="http://httpbin.org/ip">Returns Origin IP</a></li>
+                            </ul>
+                        </body>
+                        </html>""")
+        assert s.css('h1').xpath('@class')[0].text == 'header'
+        assert s.css('li > a[class=link]').xpath('@href')[0].text == 'http://httpbin.org/ip'
+        assert s.css('h1').attr('class')[0] == 'header'
+        assert s.css('h1')[0].attr('class') == 'header'
+        assert s.css('li > a').attr('class')[0] == 'primary link'
+        assert s.css('li > a').attr('class')[-1] == 'link'
+        assert s.css('ul').attr('class') == [None]
+        assert s.css('ul')[0].attr('class') is None
+        assert s.css('h2').attr('class') == []
--- xpaw/selector.py
+++ xpaw/selector.py
@@ -1,16 +1,46 @@
 # coding=utf-8
 
-from lxml import etree
+from lxml import etree, cssselect
+
+_doctype_config = {
+    'html': {
+        'parser_cls': etree.HTMLParser,
+        'tostring_method': 'html',
+        'css_translator': cssselect.LxmlHTMLTranslator()
+    },
+    'xml': {
+        'parser_cls': etree.XMLParser,
+        'tostring_method': 'xml',
+        'css_translator': cssselect.LxmlTranslator()
+    }
+}
+
+
+def _get_doctype(doctype):
+    if doctype is None:
+        return 'html'
+    if doctype in _doctype_config:
+        return doctype
+    raise ValueError('Invalid document type: {}'.format(doctype))
+
+
+def create_root_node(text, parser_cls, base_url=None):
+    return etree.fromstring(text, parser=parser_cls(), base_url=base_url)
 
 
 class Selector:
-    def __init__(self, html=None, root=None):
-        if html is not None:
-            if not isinstance(html, str):
-                raise TypeError("'html' argument should be of type {}".format(str))
-            root = etree.HTML(html)
+    def __init__(self, text=None, root=None, doctype=None, base_url=None):
+        self.type = _get_doctype(doctype)
+        c = _doctype_config[self.type]
+        self._parser_cls = c['parser_cls']
+        self._tostring_method = c['tostring_method']
+        self._css_translator = c['css_translator']
+        if text is not None:
+            if not isinstance(text, str):
+                raise TypeError("html argument must be a str")
+            root = create_root_node(text, parser_cls=self._parser_cls, base_url=base_url)
         elif root is None:
-            raise ValueError("Selector needs either 'html' or 'root' argument")
+            raise ValueError("Needs either text or root argument")
         self._root = root
 
     def xpath(self, xpath, **kwargs):
@@ -20,12 +50,26 @@ class Selector:
             return SelectorList([])
         if not isinstance(res, list):
             res = [res]
+            raise RuntimeError
+        if not isinstance(res, list):
+            return res
+        return SelectorList([self.__class__(root=i) for i in res])
+
+    def css(self, css, **kwargs):
+        try:
+            path = self._css_translator.css_to_xpath(css)
+            xpath = etree.XPath(path, smart_strings=False, **kwargs)
+            res = xpath(self._root)
+        except Exception:
+            return SelectorList([])
+        if not isinstance(res, list):
+            res = [res]
         return SelectorList([self.__class__(root=i) for i in res])
 
     @property
-    def html(self):
+    def string(self):
         try:
-            return etree.tostring(self._root, encoding="unicode", method="html", with_tail=False)
+            return etree.tostring(self._root, encoding="unicode", method=self._tostring_method, with_tail=False)
         except TypeError:
             return str(self._root)
 
@@ -35,6 +79,11 @@ class Selector:
             return etree.tostring(self._root, encoding="unicode", method="text", with_tail=False)
         except TypeError:
             return str(self._root)
+
+    def attr(self, name):
+        res = self.xpath('@' + name)
+        if len(res) > 0:
+            return res[0].text
 
 
 class SelectorList(list):
@@ -48,10 +97,19 @@ class SelectorList(list):
             res += i.xpath(xpath, **kwargs)
         return res
 
+    def css(self, css, **kwargs):
+        res = self.__class__()
+        for i in self:
+            res += i.css(css, **kwargs)
+        return res
+
     @property
-    def html(self):
-        return [i.html for i in self]
+    def string(self):
+        return [i.string for i in self]
 
     @property
     def text(self):
         return [i.text for i in self]
+
+    def attr(self, name):
+        return [i.attr(name) for i in self]
